0e3b65e8880aab9cc8353343c24d6b8df40e78c0
Naresh Choudhary <58872658+nareshvishram@users.noreply.github.com>
2020-12-07 12:42:14 +0530

    Update views.py

--- Employee/views.py
+++ Employee/views.py
@@ -1,8 +1,6 @@
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from .models import *
-import requests as req
-import json
 # Create your views here.
 
 def Home(request):
@@ -24,34 +22,3 @@ def Remove(request,emp_id):
     emp_data=Employee.objects.get(id=emp_id)
     emp_data.delete()
     return redirect("home")
-
-
-def jprint(obj):
-    # create a formatted string of the Python JSON object
-
-    text = json.dumps(obj, sort_keys=True, indent=4)
-    print(text)
-
-class BearerAuth(req.auth.AuthBase):
-    def __init__(self, token):
-        self.token = token
-    def __call__(self, r):
-        r.headers["authorization"] = "Bearer " + self.token
-        return r
-
-def api(request):
-    endpoint="https://f0ztti2nsk.execute-api.ap-south-1.amazonaws.com/v1/consignment/fetch"
-    BearerToken="tTU3gFVUdP"
-    body={"email":"mayankmittal@intugine.com"}
-    param="mayank"
-    header={
-          "Authorization":'Bearer tTU3gFVUdP',
-            }
-
-    try:
-       data=req.post(endpoint,auth =('email', 'mayankmittal@intugine.com'),headers=header,verify=True)
-       print(data.text)
-    except req.exceptions.ConnectionError:
-        req.status_code = "Connection refused"
-
-    return HttpResponse("Passed")
